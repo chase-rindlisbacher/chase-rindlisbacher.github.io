997bd54fa11c26a717d10bb1c394e3ef0ac1cc0f
chase.rindlisbacher <cjrjazz20@gmail.com>
2025-04-16 20:11:48 -0600

    hopefully deployed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "LOTR-Mini-Games",
+  base: "/chase-rindlisbacher.github.io/",
   plugins: [react()],
 })
